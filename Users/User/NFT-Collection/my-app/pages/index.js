e4d8049052045741b977d9701872d938034c184e
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-06-14 16:08:34 +0100

    Add true

--- Users/User/NFT-Collection/my-app/pages/index.js
+++ Users/User/NFT-Collection/my-app/pages/index.js
@@ -7,15 +7,15 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
-  const [walletConnected, setWalletConnected] = useState(false);
+  const [walletConnected, setWalletConnected] = useState(true);
   // presaleStarted keeps track of whether the presale has started or not
   const [presaleStarted, setPresaleStarted] = useState(true);
   // presaleEnded keeps track of whether the presale ended
-  const [presaleEnded, setPresaleEnded] = useState(false);
+  const [presaleEnded, setPresaleEnded] = useState(true);
   // loading is set to true when we are waiting for a transaction to get mined
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   // checks if the currently connected MetaMask wallet is the owner of the contract
-  const [isOwner, setIsOwner] = useState(false);
+  const [isOwner, setIsOwner] = useState(true);
   // tokenIdsMinted keeps track of the number of tokenIds that have been minted
   const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
   // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
