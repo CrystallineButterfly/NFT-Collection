7c14751f06e9ee6625ebeeeaf85e8183da3cb97d
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-06-14 15:33:44 +0100

    Add NFT-Collection to github

---
+++ Users/User/NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf833d015780bca69d518b78013c63a134254ec8b";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ Users/User/NFT-Collection/my-app/constants/index.js
@@ -0,0 +1,17 @@
+export const abi = {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "baseURI",
+          "type": "string"
+        },
+        {
+          "internalType": "address",
+          "name": "whitelistContract",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    }
+export const NFT_CONTRACT_ADDRESS = "0x2956b9c4B3FB8288C3662c6ACdBa3dEb45fD0CeF"
